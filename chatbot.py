21f2641c70283e8a798db2caf9d804d57a9e733a
Evann Courdier <evann.courdier@epfl.ch>
2024-04-10 12:29:16 +0200

    Add cost estimate

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ import base64
 
 import openai
 import streamlit as st
+import tiktoken
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     AIMessage,
@@ -29,6 +30,8 @@ def generate_response():
 
     message = AIMessage(content=[full_response])
     st.session_state.messages.append(message)
+    
+    st.session_state.cost = st.session_state.get("cost", 0) + estimate_cost(messages)
 
     try:
         for response in st.session_state.chatbot.stream(messages):
@@ -57,6 +60,29 @@ def print_message(lc_message):
             else:
                 st.image(lc_message.content[0]["image_url"]["url"])
 
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+def estimate_cost(messages):
+    cost = 0
+    
+    multiplier = {
+        "gpt-3.5-turbo": 0.5,
+        "gpt-4-turbo": 10
+    }[st.session_state.chatbot.model_name] # Per 1M tokens
+    
+    for message in messages:
+        if message.content[0]["type"] == "text":
+            num_tokens = num_tokens_from_string(message.content[0]["text"], "cl100k_base")
+            cost += num_tokens * multiplier * (1 if get_role(message) != "assistant" else 3) * 1e-6
+        elif message.content[0]["type"] == "image_url":
+            cost += 300 * 1e-6
+            
+    return cost
+
 
 def st_chatbot(openai_api_key):
     if openai_api_key == st.secrets["PASSWORD"]:
@@ -67,7 +93,7 @@ def st_chatbot(openai_api_key):
         
     if ("chatbot" not in st.session_state) or (openai_api_key != st.session_state.chatbot.openai_api_key):
         st.session_state.chatbot = ChatOpenAI(max_tokens=2048, openai_api_key=openai_api_key)
-                
+
     openai_models = st.sidebar.radio("Select OpenAI model:", ["gpt-3.5-turbo", "gpt-4-turbo"], index=1)
     st.session_state.chatbot.model_name = openai_models
 
@@ -79,7 +105,7 @@ def st_chatbot(openai_api_key):
     with st.sidebar:
         st.text_area("System message", key="system_message", value="")
         st.session_state.chatbot.temperature = st.slider("Temperature", min_value=0.0, max_value=2.0, value=1.0, step=0.05)
-        st.button("Reset chat", on_click=lambda: st.session_state.pop("messages", None), type="primary", use_container_width=True)
+        st.button("Reset chat", on_click=lambda: st.session_state.pop("messages", None) and st.session_state.pop("cost", None), type="primary", use_container_width=True)
         regenerate = st.button("Regenerate response", on_click=lambda: st.session_state.messages.pop(), use_container_width=True)
         st.button("Delete last", on_click=lambda: st.session_state.messages.pop(), use_container_width=True)
         stop = st.button("Stop", use_container_width=True)
@@ -101,23 +127,27 @@ def st_chatbot(openai_api_key):
                 message = HumanMessage(content=content)
                 st.session_state.messages.append(message)
 
-
     st.title("ChatGPT UI")
-
-
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        print_message(message)
-        
-
+    
+    container = st.container()
+    # container = st.container(height=550, border=None)
+    
+    with container:
+        for message in st.session_state.messages:
+            print_message(message)
+            
     # React to user input
-    if (prompt := st.chat_input("What is up?")) and not stop:
+    if (prompt := st.chat_input(f"Write here...")) and not stop:
         message = HumanMessage(content=[{
                 "type": "text",
                 "text": prompt,
         }])
         st.session_state.messages.append(message)
-        print_message(message)
-        generate_response()
+        with container:
+            print_message(message)
+            generate_response()
     elif regenerate and not stop:
-        generate_response()
+        with container:
+            generate_response()
+    
+    st.markdown(f"*Cost: ${st.session_state.get('cost', 0):.5f}*")
